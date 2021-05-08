8e40fdf09e8845b5147ac97db52b975389d8970f
VM <4952512+vm-solusi@users.noreply.github.com>
2021-05-08 22:53:13 +0700

    Update paper size

--- resources/views/undangan/pdfs/undangan.blade.php
+++ resources/views/undangan/pdfs/undangan.blade.php
@@ -13,7 +13,7 @@
     <style>@page { size: A5 landscape }</style>
 </head>
     <body class="A5 landscape">
-        <section class="sheet padding-25mm">
+        <section class="sheet padding-10mm" style="padding-top: 25mm;">
             <div class="clearfix">
                 <div class="logo">
                     <span class="laura">N</span>
