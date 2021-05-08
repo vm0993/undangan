fc4ba440268ae67a30c6b84706cb95e6e4310d73
VM <4952512+vm-solusi@users.noreply.github.com>
2021-05-08 21:56:44 +0700

    Add Paper size A5 x

--- resources/views/undangan/pdfs/undangan.blade.php
+++ resources/views/undangan/pdfs/undangan.blade.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta id="token" name="token" value="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/style-undangan.css') }}" media="all" />
     <link rel="stylesheet" href="{{ asset('css/style-new.css') }}" media="all" />
 </head>
     <body>
